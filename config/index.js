fdda25c55fdfbd5b62a69ad8f9175f9b95a6323a
George Lymperopoulos <glympe77@gmail.com>
2020-07-17 14:15:52 +0300

    update details

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   title: `Georgios Lymperopoulos | Front End Developer`,
   siteUrl: "http://glympero-portfolio.herokuapp.com/",
-  description: `I am a highly motivated Front End Developer based in London, United Kingdom, who is passionate about creating rich user interfaces and interactive web applications using a wide range of JavaScript technologies and libraries.
-    I strongly believe in the importance of writing clean, modular and loosely coupled code that is easy to document, maintain and reuse.
-    I have experience working with HTML, CSS, JavaScript, React, Redux, TypeScript, GraphQL, Apollo, Electron, Jest, Webpack.`,
+  description: `I am a highly motivated Front End Developer based in Arta, Greece, mainly using a wide range of JavaScript technologies and libraries.
+    I have experience working with HTML, CSS, JavaScript, React, Redux, TypeScript, GraphQL, Apollo, Angular, Jest, Webpack, Node, PHP, Wordpress, Drupal.
+    `,
   keywords:
     "Georgios Lymperopoulos, Lymperopoulos, glympe, lympero, glympe77, Software Engineer, Front End Developer, Front End Engineer, developer, software developer, web developer, javascript, greece, remote",
   author: `Georgios Lymperopoulos`,
